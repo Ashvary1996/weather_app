b8239c5643a24f63ec2355f5d024be1d4aafd9ad
Ashvary Gidian <ashvarygidian1996@gmail.com>
2023-10-20 02:39:42 +0530

    search bar activated + last 4 searched show

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -6,15 +6,18 @@ import $ from "jquery";
 function CurrentWeather() {
   const [data, setData] = useState(null);
   const [forecast, setForecast] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [lastSearch, setLastSearch] = useState([]);
 
   const fetchData = async () => {
     await axios
       .get(
-        `https://api.weatherapi.com/v1/forecast.json?key=${dotenv.API_KEY}&q=Chhindwara&days=7&aqi=yes&alerts=yes`
+        `https://api.weatherapi.com/v1/forecast.json?key=${dotenv.API_KEY}&q=${search}&days=7&aqi=yes&alerts=yes`
       )
       .then((res) => {
         setData(res.data);
         setForecast(data.forecast.forecastday);
+        setLastSearch([...lastSearch, search]);
       })
       .catch((err) => console.log(err));
   };
@@ -24,15 +27,22 @@ function CurrentWeather() {
     $(".forecastText").text("Hide Forecast");
     $(".showForecast").fadeToggle();
   };
-  console.log("data", data);
-  console.log("forecast", forecast);
+  const getValue = (e) => {
+    console.log("Clicked on  ", e.target.value);
+  };
+  // console.log(search);
+  // console.log("data", data);
+  // console.log("forecast", forecast);
   return (
     <>
       {data == null ? (
         <>
           <h1>Welcome to Weather App </h1>
-
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
           <button onClick={fetchData}>Search</button>
         </>
       ) : (
@@ -102,20 +112,38 @@ function CurrentWeather() {
                 <input
                   className="border pl-3 text-black"
                   type="text"
+                  onChange={(e) => setSearch(e.target.value)}
+                  value={search}
                   placeholder="search location"
                 />
                 <button className="border" onClick={fetchData}>
-                  {" "}
                   Search
                 </button>
               </div>
 
               {/* Render this Div conditinally */}
               <div className="text-sm text-left pl-5 mt-2">
-                <p>Chhindwara</p>
-                <p>Bhopal</p>
-                <p>Jabalpur</p>
-                <p>Indore</p>
+                {lastSearch && lastSearch.length <= 4 ? (
+                  <>
+                    {lastSearch.map((city, i) => (
+                      <p
+                        key={i}
+                        id={i + 1 + "city"}
+                        className="cursor-pointer"
+                        // value={city}
+                        onClick={(e) => {
+                          console.log(e.target.innerText);
+                          setSearch(e.target.innerText);
+                          // fetchData();
+                        }}
+                      >
+                        {city}
+                      </p>
+                    ))}
+                  </>
+                ) : (
+                  setLastSearch([])
+                )}
               </div>
 
               <div></div>
@@ -123,23 +151,49 @@ function CurrentWeather() {
               <hr className="border mt-5" />
 
               <div className="">
-                <p>Weather Details</p>
+                <p>Weather Location Details</p>
                 <br />
                 <div className="flex flex-col  border pl-5 pr-5">
                   <div className="flex justify-between">
-                    {" "}
-                    <p>Cloudy</p>
-                    <p>86%</p>
+                    <p>City</p>
+                    <p>{data.location.name}</p>
                   </div>
                   <div className="flex justify-between">
-                    {" "}
-                    <p>Humidity</p>
-                    <p>62%</p>
+                    <p>Region</p>
+                    <p>{data.location.region}</p>
                   </div>
                   <div className="flex justify-between">
-                    {" "}
+                    <p>Country</p>
+                    <p>{data.location.country}</p>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <p>Timezone</p>
+                    <p>{data.location.tz_id}</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Lat/Lon</p>
+                    <p>{data.location.lat + " / " + data.location.lon}</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Local Time</p>
+                    <p>{data.location.localtime}</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Temperature{}</p>
+                    <p>{data.current.temp_c}&deg; C</p>
+                  </div>
+                  <div className="flex justify-between">
                     <p>Wind</p>
-                    <p></p>8km/h
+                    <p>{data.current.wind_kph}km/h</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Humidity</p>
+                    <p>{data.current.humidity} </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Visibility</p>
+                    <p>{data.current.vis_km}km </p>
                   </div>
                 </div>
               </div>
